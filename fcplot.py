6a94d5246cc702d207cfe4f458223a72a5c0684a
mir3x <mlewczuk80@gmail.com>
2020-09-25 14:55:54 +0200

    Added header license to fcplot.py

--- fcplot.py
+++ fcplot.py
@@ -1,5 +1,22 @@
 #python3.6+
 
+# plots data from given .csv files
+# Copyright (C) 2020 mir3x
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
@@ -9,7 +26,7 @@ from argparse import ArgumentParser
 
 #color dict
 cold = {
-    0 : 'w', 
+    0 : 'w',
     1 : 'r',
     2 : 'g',
     3 : 'k',
@@ -20,7 +37,7 @@ cold = {
     8 : 'b',
     9 : '#ff8800',
     10: '#B22222',
-    11: '#CD5C5C', 
+    11: '#CD5C5C',
     12: '#FF6347',
     13: '#FFE4B5',
     14: '#BDB76B',
@@ -48,38 +65,38 @@ def read_file(filename):
 
 def limit_to_players(csv, plist):
     return csv.reindex(plist)
-    
+
 def main(filename, plottype, playerlist, excludelist, xlim, ylim, log_x, log_y, yname, pie_turn,
           topx, scatter, starchange, chg_inc):
-    
+
     data = read_file(filename)
     colormap = "Blues"
-    
+
     #remove unnamed
     data = data.loc[:, ~data.columns.str.contains('^Unnamed')]
-    
+
     #fill dead players score as last one
     data = data.replace(r'\s+', np.nan, regex=True)
     data = data.fillna(method='ffill')
-    
+
     #fill remaining Nan as 0
     data.fillna(0, inplace=True)
-    
-    
+
+
     if xlim != "nolimits":
         x = xlim.split(':')
         xmin = x[0]
         xmax = x[1]
         print(f"X axis min set to {xmin}, max to {xmax}")
-    
+
     if playerlist != "all":
         player_list = playerlist.split(':')
         print("Included players:", player_list)
-        
+
     if excludelist != "none":
         exclude_list = excludelist.split(':')
         print("Excommunicated players:", exclude_list)
-    
+
     if playerlist != "all":
         data = data[player_list]
     if excludelist != "none":
@@ -87,24 +104,24 @@ def main(filename, plottype, playerlist, excludelist, xlim, ylim, log_x, log_y, 
             data = data.loc[:, ~data.columns.str.contains(i)]
     if xlim != "nolimits":
         data = data[int(xmin):int(xmax)]
-    
+
     if topx > 0:
         stats = data.describe()
-            
+
         #convert mean to dict
         cols = stats.loc['mean'].to_dict()
-        
+
         #sort columns by value
         cols = {k: v for k, v in sorted(cols.items(), key=lambda item: item[1])}
-        
+
         col_list = []
         #make column list
         for k in cols.keys():
             col_list.append(k)
-        
+
         #revert list (was sorted from min)
         col_list = col_list[::-1]
-        
+
         top_list = []
         i = 0
         for item in col_list:
@@ -112,27 +129,27 @@ def main(filename, plottype, playerlist, excludelist, xlim, ylim, log_x, log_y, 
             top_list.append(item)
             if i >= topx:
                 break
-        
-        
+
+
         print("Top players:", top_list)
         data = data[top_list]
         stats = data.describe()
         print(stats)
-                
+
     if pie_turn != -1:
         plottype = "pie"
 
-    
+
     if ylim != "nolimits":
         y = ylim.split(':')
         ymin = y[0]
         ymax = y[1]
         print(f"Y axis min set to {ymin}, max to {ymax}")
-                
+
     if yname != "":
         filename = yname
-        
-     
+
+
     #array kicked
     dt = data.T
 
@@ -140,14 +157,14 @@ def main(filename, plottype, playerlist, excludelist, xlim, ylim, log_x, log_y, 
         data3d = read_file(starchange)
         data.fillna(0, inplace=True)
         data3d.fillna(0, inplace=True)
-        
+
         if xlim != "nolimits":
             data3d = data3d[int(xmin):int(xmax)]
-        
+
         dd = data3d.to_numpy()
         ddcopy = dd
-        
-        
+
+
         xlen = len(dd) # 144
         ylen = len(dd[0]) #19
         for y in range(ylen):
@@ -158,14 +175,14 @@ def main(filename, plottype, playerlist, excludelist, xlim, ylim, log_x, log_y, 
                     dd[x,y] = 0
                 # put it at end to show at end of graph
                 if (val != 0):
-                    dd[xlen - 1, y] = val 
+                    dd[xlen - 1, y] = val
                 old = val
-        
-            
+
+
         z = data.plot(xlabel="turn", ylabel=filename, logx=log_x, logy = log_y)
         if ylim != "nolimits":
             z.set_ylim(int(ymin), int(ymax))
-        
+
         xlen = len(dd) # 144
         ylen = len(dd[0]) #19
         for y in range(ylen):
@@ -174,15 +191,15 @@ def main(filename, plottype, playerlist, excludelist, xlim, ylim, log_x, log_y, 
                     z = data.iloc[x,y]
                     col_nr = dd[x,y] % 31  # 31 is number of all colors
                     plt.plot(x, z, '*', color=cold[col_nr])
-        
+
         plt.show()
         exit(0)
-        
-        
+
+
     if chg_inc:
         colormap = 'Reds'
         data_copy = data
-        xlen = len(data) # rows 
+        xlen = len(data) # rows
         ylen = len(data.columns) #cols
 
         print(xlen)
@@ -194,19 +211,19 @@ def main(filename, plottype, playerlist, excludelist, xlim, ylim, log_x, log_y, 
                 if z >= old:
                     data_copy.iloc[x, y] = z - old
                 old = z
-        
+
         data = data_copy
-        
+
     if scatter != "none":
         data3d = read_file(scatter)
         data3d = data3d.loc[:, ~data3d.columns.str.contains('^Unnamed')]
-       
+
         if xlim != "nolimits":
             data3d = data3d[int(xmin):int(xmax)]
 
         print(data.head())
         print(data3d.head())
-        
+
         xlen = len(data) # rows
         ylen = len(data.columns) #cols
 
@@ -216,51 +233,51 @@ def main(filename, plottype, playerlist, excludelist, xlim, ylim, log_x, log_y, 
                 xz = data.iloc[x,y]
                 col_nr = z % 31
                 plt.plot(xz, z, '*', color=cold[col_nr])
-        
+
         #plt.scatter(data, data3d)
         plt.gca().set(xlabel=filename, ylabel=scatter)
         plt.show()
         exit(0)
-    
+
     if plottype == 'percentage':
 
         rows = dt
         rows = (100. * rows / rows.sum()).round(2)
         rows = rows.T
-                    
+
         z = rows.plot.area(xlabel="turn", ylabel=filename, logx=log_x, logy = log_y)
-        
+
         if ylim != "nolimits":
             z.set_ylim(int(ymin), int(ymax))
-            
+
         plt.legend(loc='upper left')
         plt.show()
-            
+
     if plottype == 'simple':
-        
+
         z = data.plot(xlabel="turn", ylabel=filename, logx=log_x, logy = log_y)
         if ylim != "nolimits":
             z.set_ylim(int(ymin), int(ymax))
         plt.show()
 
     if plottype == 'heatmap':
-        
+
         dx = data.T
         plt.imshow(data.T, cmap =colormap)
         plt.colorbar()
-        plt.xticks(range(len(data)), data.index) 
+        plt.xticks(range(len(data)), data.index)
         plt.yticks(range(len(data.columns)), data.columns)
         plt.show()
-        
+
         # split and add labels ?
-        # data1 = data[0:75] 
+        # data1 = data[0:75]
         # data2 = data[75:len(data)]
         # fig, axes = plt.subplots(nrows=2, ncols=1)
         # im3 = axes[0].imshow(data1.T)
         # im4 = axes[1].imshow(data2.T)
 
     if plottype == 'hellokitty':
-        
+
         plt.text(0.1,0.5, "Hello", fontsize=100)
         plt.text(0.14,0.1, "Kitty", fontsize=100)
         plt.show()
@@ -276,7 +293,7 @@ def main(filename, plottype, playerlist, excludelist, xlim, ylim, log_x, log_y, 
         rows = rows[pie_turn]
         z = rows.plot.pie(ylabel=("turn " + str(pie_turn)))
         plt.show()
-            
+
 if __name__ == '__main__':
     parser = ArgumentParser(description='Plot csv files')
     parser.add_argument('filename',nargs='?', default='', help='csv file to read')
@@ -307,5 +324,4 @@ if __name__ == '__main__':
     args = parser.parse_args()
     main(args.filename, args.type, args.playerlist, args.excludelist, args.xlim, args.ylim, args.logx,
          args.logy, args.yname, args.pie, args.top, args.scatter, args.starchange, args.chg_inc)
-    
-    
+
